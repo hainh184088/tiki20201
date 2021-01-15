b47d3dc2db6cb602e52e34a3f351ebe11baad1a4
Hai Nguyen <thomas.nguyen747@gmail.com>
2021-01-15 13:44:28 +0700

    change settings.py

--- tiki20201/settings.py
+++ tiki20201/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'tiki20201.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'end_term',
-        # 'USER': 'root',
-        # 'PASSWORD': 'plantsynchro123',
-        # 'HOST':'127.0.0.1',
-        # 'PORT':'3306'
+        'NAME': 'end_term',
+        'USER': 'root',
+        'PASSWORD': 'plantsynchro123',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
 
@@ -144,3 +144,6 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
